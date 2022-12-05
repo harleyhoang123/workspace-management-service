78c0b43133831fa010989711027a3403cb65233f
vaduc <vaduc154@gmail.com>
2022-12-05 23:51:07 +0700

    fix convertSprintToGetSprintResponse

--- src/main/java/vn/edu/fpt/workspace/service/impl/SprintServiceImpl.java
+++ src/main/java/vn/edu/fpt/workspace/service/impl/SprintServiceImpl.java
@@ -181,12 +181,14 @@ public class SprintServiceImpl implements SprintService {
         return GetSprintResponse.builder()
                 .sprintId(sprint.getSprintId())
                 .sprintName(sprint.getSprintName())
+                .goal(sprint.getGoal())
                 .status(sprint.getStatus())
                 .startDate(sprint.getStartDate())
                 .endDate(sprint.getEndDate())
                 .totalNotStartedTask(totalNotStartedTask)
                 .totalInProgressTask(totalInProgressTask)
                 .totalDoneTask(totalDoneTask)
+                .tasks(sprint.getTasks().stream().map(this::convertTaskToGetTaskResponse).collect(Collectors.toList()))
                 .build();
     }
 
